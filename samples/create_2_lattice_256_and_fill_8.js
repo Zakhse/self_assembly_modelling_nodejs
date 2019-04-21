177595f1702e9467ad0877baf3edfdaa4aa9ceeb
Zakhse <zakhse@yandex.ru>
2019-04-21 03:33:00 +0300

    Create sample of creating lattice and filling with particles

---
+++ samples/create_2_lattice_256_and_fill_8.js
@@ -0,0 +1,12 @@
+const Lattice = require('../src/models/lattice')
+
+const t = Date.now()
+
+global.prevTime = t
+
+for (let i = 0; i < 2; i++) {
+  const l = new Lattice({ size: 256, particleLength: 8 })
+  l._fillWithParticles()
+}
+
+console.log('Time elapsed: ' + (Date.now() - t) + ' milliseconds')
